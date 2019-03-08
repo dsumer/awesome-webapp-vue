fd3d5f99665f84e30652ee83ae3a869e17729b38
Dominik Sumer <domi.sumer@gmail.com>
2019-03-08 22:25:52 +0100

    added github stargazers api calls

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,43 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        awesome-webapp-vue
-      </h1>
-      <h2 class="subtitle">
-        Example of how an awesome webapp should work in 2019. (Vue &amp; nuxt.js)
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="wrapper">
+    ahoi!
+    <br/>
+    Stars Count (Server Side): {{ serverSideStars }}
+    <br/>
+    Stars Count (Client Side): {{ clientSideStars }}
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import fetch from 'isomorphic-unfetch';
+
+const fetchStars = async () => {
+  const res = await fetch('https://api.github.com/repos/zeit/next.js')
+  const json = await res.json()
+  return json.stargazers_count
+}
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      clientSideStars: 0
+    }
+  },
+  async asyncData() {
+    const serverSideStars = await fetchStars();
+    return {
+      serverSideStars
+    }
+  },
+  async mounted() {
+    const clientSideStars = await fetchStars();
+    this.clientSideStars = clientSideStars;
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .wrapper {
+    text-align: center;
+  }
 </style>
